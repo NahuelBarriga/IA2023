fac00eacf28f299cb807f6d3fc1285880c29f4b0
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-10-04 15:59:22 -0300

    Update mamdani.py

--- TP1/mamdani.py
+++ TP1/mamdani.py
@@ -26,7 +26,7 @@ regla 3:
 # [ x and  A --> B  ] = [Umin(x,A) --> B ]
 
 # variables universales
-x_nota = np.arange(0, 110, 5)
+x_nota = np.arange(0, 100, 5)
 y_concepto = np.arange(0, 10.5, 0.5)
 a_final = np.arange(0, 10.5, 0.5)
 
@@ -42,17 +42,17 @@ excelente = np.arange(7,10,1)
 
 # ?chequear estos parametros
 
-nota_low = fuzz.trapmf(x_nota, [0, 0, 30, 50])  # nota baja: [0,30]
+nota_low = fuzz.trapmf(x_nota, [0, 0, 20, 40])  # nota baja: [0,30]
 nota_md = fuzz.trapmf(x_nota, [20, 40, 60, 80])  # nota media: [40,60]
-nota_hi = fuzz.trapmf(x_nota, [50, 70, 100, 100])  # nota alta [70,100]
+nota_hi = fuzz.trapmf(x_nota, [60, 80, 100, 100])  # nota alta [70,100]
 
-concepto_low = fuzz.trapmf(y_concepto, [0, 0, 3, 5])
-concepto_md = fuzz.trapmf(y_concepto, [2, 4, 6, 8])
-concepto_hi = fuzz.trapmf(y_concepto, [5, 7, 10, 10])
+concepto_low = fuzz.trapmf(y_concepto, [0, 0, 3, 3])
+concepto_md = fuzz.trapmf(y_concepto, [4, 4, 6, 6])
+concepto_hi = fuzz.trapmf(y_concepto, [7, 7, 10, 10])
 
-final_low = fuzz.trapmf(a_final, [0, 0, 3, 5])
+final_low = fuzz.trapmf(a_final, [0, 0, 2, 4])
 final_md = fuzz.trapmf(a_final, [2, 4, 6, 8])
-final_hi = fuzz.trapmf(a_final, [5, 7, 10, 10])
+final_hi = fuzz.trapmf(a_final, [6, 8, 10, 10])
 
 # grafico de estas funciones
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
@@ -86,15 +86,23 @@ plt.tight_layout()
 
 #! paso 2: INFERENCIA
 
-value_nota = 85
-value_concepto = 1.5
+value_nota = 90
+value_concepto = "Regular" # "Regular" "Buena" "Excelente"
 peso_nota = 5
 peso_concepto = 1
 
+metodo_defuzz = "bisector"
 
 print("nota examen:", value_nota)
 print("nota concepto:", value_concepto)
 print("")
+
+if (value_concepto == "Regular"):
+    value_concepto = 2
+elif (value_concepto == "Buena"):
+    value_concepto = 4
+else: 
+    value_concepto = 10
 
 prom_ponderado = (peso_nota * value_nota + peso_concepto * value_concepto) / (
     peso_nota + peso_concepto
@@ -134,10 +142,13 @@ active_rule1 = (peso_nota * nota_level_lo + peso_concepto * concepto_level_lo) /
 print("active_rule1", active_rule1)
 final_activation_lo = np.fmin(active_rule1, final_low)
 
-# rule 2: nota media OR concepto bueno then habilita
+# rule 2: nota media OR concepto bueno then habilita #!eliminamos el peso del concepto
+"""
 active_rule2 = (peso_nota * nota_level_md + peso_concepto * concepto_level_md) / (
     peso_nota + peso_concepto
 )
+"""
+active_rule2 = nota_level_md
 print("active_rule3", active_rule2)
 
 final_activation_md = np.fmin(active_rule2, final_md)
@@ -194,7 +205,7 @@ print("agregated", aggregated)
 
 #! paso 4: DEFUZZIFICACION
 
-final = fuzz.defuzz(a_final, aggregated, "centroid")
+final = fuzz.defuzz(a_final, aggregated, metodo_defuzz)
 final_activation = fuzz.interp_membership(a_final, aggregated, final)
 print(f"------------ NOTA FINAL:{final} --------------")
 
